0ae5e0d7b9c5f022ff6b942e632cd981842fbc86
vkoschel <victorkoschel@gmail.com>
2021-10-17 11:45:03 -0300

    functionally testing button color and text change

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [btnColor, setBtnColor] = useState("red");
+  const newBtnColor = btnColor === "red" ? "green" : "red";
   return (
-  <div>
-    <button style={{backgroundColor: 'red'}}>Change to green</button>
-  </div>
+    <div>
+      <button
+        style={{ backgroundColor: btnColor }}
+        onClick={() => setBtnColor(newBtnColor)}
+      >
+        Change to {newBtnColor}
+      </button>
+    </div>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,15 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./App";
 
 test("button has correct initial colour", () => {
   render(<App />);
   const button = screen.getByRole("button", { name: "Change to green" });
   expect(button).toHaveStyle({ backgroundColor: "red" });
+  fireEvent.click(button);
+  expect(button).toHaveStyle({ backgroundColor: "green" });
+  expect(button.textContent).toBe("Change to red");
 });
-test("button turns green when cliclekd", () => {});
+test("button turns green when clicled", () => {
+  render(<App />);
+});
 test("button has correct initial colour", () => {});
