901cf73b2fb487263fa4f4057c9b9309509a5ae0
vkoschel <victorkoschel@gmail.com>
2022-01-23 09:05:26 -0300

    opt 3: disabled button if empty order test

--- src/pages/entry/OrderEntry.jsx
+++ src/pages/entry/OrderEntry.jsx
@@ -5,12 +5,16 @@ import Button from 'react-bootstrap/Button';
 export default function OrderEntry({ setOrderPhase }) {
   const [orderDetails] = useOrderDetails();
 
+  const orderDisabled = orderDetails.totals.scoops === '$0.00';
+
   return (
     <div>
       <Options optionType="scoops" />
       <Options optionType="toppings" />
       <h2> grand total: {orderDetails.totals['grandTotal']}</h2>
-      <Button onClick={() => setOrderPhase('review')}>Order Sundae!</Button>
+      <Button disabled={orderDisabled} onClick={() => setOrderPhase('review')}>
+        Order Sundae!
+      </Button>
     </div>
   );
 }
--- src/pages/entry/tests/OrderEntry.test.jsx
+++ src/pages/entry/tests/OrderEntry.test.jsx
@@ -3,6 +3,8 @@ import {
   screen,
   waitFor,
 } from '../../../test-utils/testing-library-utils';
+import userEvent from '@testing-library/user-event';
+
 import OrderEntry from '../OrderEntry';
 
 import { rest } from 'msw';
@@ -24,4 +26,24 @@ test('handle error for scoops and toppings routes', async () => {
     const alerts = await screen.findAllByRole('alert');
     expect(alerts).toHaveLength(2);
   });
+});
+
+test('disable order button if the order is empty', async () => {
+  render(<OrderEntry />);
+
+  let orderButton = screen.getByRole('button', { name: /order sundae/i });
+  expect(orderButton).toBeDisabled();
+
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: /vanilla/i,
+  });
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '1');
+
+  expect(orderButton).toBeEnabled();
+
+  userEvent.clear(vanillaInput);
+  userEvent.type(vanillaInput, '0');
+
+  expect(orderButton).toBeDisabled();
 });
